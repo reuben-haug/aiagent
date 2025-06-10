fec9ddf24c4db65196ff34eb6cff2054b74b0af0
Reuben Haug <reuben.haug@gmail.com>
2025-06-10 11:27:12 -0700

    Chapter 1 - Lesson 5

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+
 if len(sys.argv) != 2:
     print(f"Usage: python3 main.py 'your prompt here'")
     sys.exit(1)
+
 input_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", 
+    parts=[types.Part(text=input_prompt)])
+]
+
 # GenerateContentResponse object assignment
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=input_prompt
+    contents=messages
 )
 
 print(response.text)
