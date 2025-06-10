0f47a1b47d8c0e4c8edde4868748aebfcbbff69c
Reuben Haug <reuben.haug@gmail.com>
2025-06-10 11:15:54 -0700

    Chapter 1 - Lesson 4

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # main.py
 
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-
+if len(sys.argv) != 2:
+    print(f"Usage: python3 main.py 'your prompt here'")
+    sys.exit(1)
+input_prompt = sys.argv[1]
 # GenerateContentResponse object assignment
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=input_prompt
 )
 
 print(response.text)
